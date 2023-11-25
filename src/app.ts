f3f9da3fc914dc1ed9a3d391631570e3f49c4019
sagor66 <sazzadulhaquesagor@gmail.com>
2023-11-25 20:59:48 +0600

    completed config and basic setup

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express, { Application } from 'express';
+import cors from 'cors';
+
+const app: Application = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use('/api/users', UserRoutes);
+
+export default app;
